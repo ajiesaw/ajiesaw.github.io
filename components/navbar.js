89da5bb2ed1e497c67ddfefe547a64426dc3393e
ajiesaw <ajiesaw1612@gmail.com>
2022-11-17 19:59:26 +0700

    Add responsive

--- components/navbar.js
+++ components/navbar.js
@@ -2,17 +2,17 @@ export default function Navbar() {
   return (
     <div className=" flex justify-between ">
       <button className=" text-white text-3xl ">AjieSAW</button>
-      <ul className=" flex text-white items-center gap-x-10 ">
-        <button className=" hover:text-indigo-500 duration-100 ">Home</button>
+      <ul className=" hidden sm:flex text-white items-center px-10 gap-x-5 md:gap-x-10 justify-center ">
+        <button className=" hover:text-indigo-500 duration-100 hidden md:flex ">Home</button>
         <button className=" hover:text-indigo-500 duration-100 ">Music</button>
         <button className=" hover:text-indigo-500 duration-100 ">Photography</button>
         <button className=" hover:text-indigo-500 duration-100 ">Programming</button>
       </ul>
-      <label className=' flex gap-x-1 items-center '>
+      <label className=' flex gap-x-1 items-center justify-end '>
         <input
           type='text'
           placeholder='Search...'
-          className=' text-slate-900 h-4/5 px-2 bg-white border rounded-md border-gray-300 focus:outline-none focus:border-sky-500'
+          className=' text-slate-900 w-1/2 sm:w-full h-4/5 px-2 bg-white border rounded-md border-gray-300 focus:outline-none focus:border-sky-500'
         />
         <button className="text-white bg-indigo-700 font-bold h-4/5 px-2 rounded-md hover:bg-indigo-600 duration-100 active:bg-indigo-800">Search</button>
     </label>
