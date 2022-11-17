13be65b391663a0aed0045f4e3e9cbab7b88de91
ajiesaw <ajiesaw1612@gmail.com>
2022-11-17 19:02:55 +0700

    Change navbar to button with hover

--- components/navbar.js
+++ components/navbar.js
@@ -1,12 +1,12 @@
 export default function Navbar() {
   return (
     <div className=" flex justify-between ">
-      <div className=" text-white text-3xl ">AjieSAW</div>
+      <button className=" text-white text-3xl ">AjieSAW</button>
       <ul className=" flex text-white items-center gap-x-10 ">
-        <li>Home</li>
-        <li>Music</li>
-        <li>Photography</li>
-        <li>Programming</li>
+        <button className=" hover:text-indigo-500 duration-100 ">Home</button>
+        <button className=" hover:text-indigo-500 duration-100 ">Music</button>
+        <button className=" hover:text-indigo-500 duration-100 ">Photography</button>
+        <button className=" hover:text-indigo-500 duration-100 ">Programming</button>
       </ul>
       <label className=' flex gap-x-1 '>
         <input
@@ -14,7 +14,7 @@ export default function Navbar() {
           placeholder='Search...'
           className=' text-slate-900 h-4/5 px-2 bg-white border rounded-md border-gray-300 focus:outline-none focus:border-sky-500'
         />
-        <button className="text-white bg-indigo-700 font-bold h-4/5 px-2 rounded-md hover:bg-indigo-600 active:bg-indigo-800">Search</button>
+        <button className="text-white bg-indigo-700 font-bold h-4/5 px-2 rounded-md hover:bg-indigo-600 duration-100 active:bg-indigo-800">Search</button>
     </label>
     </div>
   )
