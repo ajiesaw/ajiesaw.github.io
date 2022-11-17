99d7359cf534f556eee231f2c4a76f152efa81a9
ajiesaw <ajiesaw1612@gmail.com>
2022-11-17 18:29:05 +0700

    Add navbar

--- components/navbar.js
+++ components/navbar.js
@@ -1,7 +1,13 @@
 export default function Navbar() {
   return (
-    <>
-      <span className=" text-white text-3xl ">AjieSAW</span>
-    </>
+    <div className=" flex ">
+      <div className=" text-white text-3xl ">AjieSAW</div>
+      <ul className=" flex text-white items-center ">
+        <li>Home</li>
+        <li>Music</li>
+        <li>Photography</li>
+        <li>Programming</li>
+      </ul>
+    </div>
   )
 }
