b5403891adc9a895b644ca591c4eaae309718049
ajiesaw <ajiesaw1612@gmail.com>
2022-11-17 18:51:46 +0700

    Modify search

--- components/navbar.js
+++ components/navbar.js
@@ -8,7 +8,14 @@ export default function Navbar() {
         <li>Photography</li>
         <li>Programming</li>
       </ul>
-      <div className=" flex text-white items-center ">Search</div>
+      <label className=' flex gap-x-1 '>
+        <input
+          type='text'
+          placeholder='Search...'
+          className=' text-slate-900 h-4/5 px-2 bg-white border rounded-md border-gray-300 focus:outline-none focus:border-sky-500'
+        />
+        <button className="text-white bg-indigo-700 font-bold h-4/5 px-3 rounded-md hover:bg-indigo-600 active:bg-indigo-800">Search</button>
+    </label>
     </div>
   )
 }
