1bea8a7e2dad376fbe0f9c649a4443c3b8897494
ajiesaw <ajiesaw1612@gmail.com>
2022-11-17 18:53:29 +0700

    Add gap

--- components/navbar.js
+++ components/navbar.js
@@ -2,7 +2,7 @@ export default function Navbar() {
   return (
     <div className=" flex justify-between ">
       <div className=" text-white text-3xl ">AjieSAW</div>
-      <ul className=" flex text-white items-center ">
+      <ul className=" flex text-white items-center gap-x-10 ">
         <li>Home</li>
         <li>Music</li>
         <li>Photography</li>
@@ -14,7 +14,7 @@ export default function Navbar() {
           placeholder='Search...'
           className=' text-slate-900 h-4/5 px-2 bg-white border rounded-md border-gray-300 focus:outline-none focus:border-sky-500'
         />
-        <button className="text-white bg-indigo-700 font-bold h-4/5 px-3 rounded-md hover:bg-indigo-600 active:bg-indigo-800">Search</button>
+        <button className="text-white bg-indigo-700 font-bold h-4/5 px-2 rounded-md hover:bg-indigo-600 active:bg-indigo-800">Search</button>
     </label>
     </div>
   )
