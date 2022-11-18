e84a3e53a812c72b025b01669eb907f99af1e4bc
ajiesaw <ajiesaw1612@gmail.com>
2022-11-18 17:31:00 +0700

    Add footer text

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,15 @@
+import Link from "next/link";
+
 export default function Footer() {
+  const date = new Date();
+  const year = date.getFullYear();
+
   return (
     <>
       <div className=" border-t-2 border-slate-800 "></div>
+      <p className=" text-white flex justify-center pt-5 ">
+        ©&nbsp;{year}&nbsp;<Link className=" hover:text-indigo-500 active:text-indigo-800 duration-100 " href={'https://github.com/ajiesaw'}>AjieSAW</Link>
+      </p>
     </>
   )
 }
